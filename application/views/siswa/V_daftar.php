83461f8e703fd2fb9ab48e08abee7bead878891a
fourirakbar <fourir.akbar@gmail.com>
2016-05-27 05:13:33 +0700

    ads

---
+++ application/views/siswa/V_daftar.php
@@ -0,0 +1,45 @@
+<div class="col-sm-9 col-sm-offset-3 col-lg-10 col-lg-offset-2 main">					 
+		<div class="row">
+			<ol class="breadcrumb">
+				<li><a href="#"><svg class="glyph stroked home"><use xlink:href="#stroked-home"></use></svg></a></li>
+				<li class="active">Icons</li>
+			</ol>
+		</div><!--/.row-->
+		
+		<div class="row">
+			<div class="col-lg-12">
+				<h1 class="page-header">Daftar Sekolah</h1>
+			</div>
+		</div><!--/.row-->
+				
+		
+		<div class="row">
+			<div class="col-lg-12">
+				<div class="panel panel-default">
+					<div class="panel-heading">Form Elements</div>
+					<div class="panel-body">
+						<div class="col-md-6">
+							<form role="form">
+							
+								<div class="form-group">
+									<label>Pilih Sekolah</label>
+									<input class="form-control" placeholder="Pilih Sekolah">
+								</div>
+
+
+								
+								<button type="submit" class="btn btn-primary">Submit Button</button>
+							</div>
+						</form>
+					</div>
+				</div>
+			</div><!-- /.col-->
+		</div><!-- /.row -->
+		
+		
+		
+	</div><!--/.main-->
+								
+			</div><!--/.col-->
+		</div><!--/.row-->
+	</div>	<!--/.main-->
